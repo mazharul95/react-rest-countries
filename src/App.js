a12e45e061aae5c0004e32ea3c20edc6b22a81ce
Md. Mazharul Islam <mipiyas07@gmail.com>
2023-03-31 08:10:25 +0600

    Display countries in a simple way, Folder Structure

--- src/App.js
+++ src/App.js
@@ -21,10 +21,22 @@ function LoadCountries(){
 
   return (
     <div>
-        <h1>Visiting Every Country of the World</h1>
+        <h1>Visiting Every Country of the World!!!</h1>
         <h3>available Countries: {countries.length}</h3>
+        {
+          countries.map(country => <Country name={country.name.common} population={country.population}></Country>)
+        }
     </div>
   )
+}
+function Country(props){
+  return (
+    <div>
+      <h2>name: {props.name}</h2>
+      <h4>Population: {props.population}</h4>
+    </div>
+  )
+
 }
 
 export default App;
